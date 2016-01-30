724997db5f6acb32427d0e9bd42299f46f94cc13
katieswill <katieswill@gmail.com>
2016-01-30 16:29:42 -0700

    adding all our workshop files

---
+++ conditional.py
@@ -0,0 +1,11 @@
+x = 100
+day = "Friday"
+if not x > 10:
+    print("Today is " + day + \
+         "! Finally...")
+elif day == "Tuesday":
+    print("Is it the weekend yet?!")
+else:
+    print("nevermind...")
+        
+        
---
+++ iteration.py
@@ -0,0 +1,7 @@
+# Katie Williams katieswill@gmail.com 2016/01/30 this is code to work on loops
+
+books = ["Tale of Two Cities","Happy Potter","Lord of the Rings"];
+nums = list(range(100));
+
+for book in books:
+    print(book)
